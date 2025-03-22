7bb77e50a6cb6dea40b587debd800e3d895eb9c1
Jakub Matysek <148622115+ENRAEGE@users.noreply.github.com>
2025-03-22 12:37:15 +0100

    Add files via upload

---
+++ Zadanie1/ConsoleApp1/Mag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Mag : INPC
+    {
+        void INPC.Przedstawsie()
+        {
+            Console.WriteLine("Jestem Magiem, władającym magią żywiołów");
+        }
+    }
+}
