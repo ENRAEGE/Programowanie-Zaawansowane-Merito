a38028e5528f14fc2eeac0b3c8a07d9bdeb405e7
Jakub Matysek <148622115+ENRAEGE@users.noreply.github.com>
2025-03-22 14:13:36 +0100

    Add files via upload

---
+++ Zadanie1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFabrykaNPC fabryka;
+            switch (new Random().Next(3))
+            {
+                case 0:
+                    fabryka = new FabrykaMaga();
+                    break;
+
+                case 1:
+                    fabryka = new FabrykaWojownika();
+                    break;
+
+                case 2:
+                    fabryka = new FabrykaZlodzieja();
+                    break;
+
+                default:
+                    throw new Exception("mesejdż");
+            }
+            INPC npc = fabryka.CreateNPC();
+            npc.Przedstawsie();
+        }
+    }
+
+    public interface INPC
+    {
+        void Przedstawsie();
+    }
+
+    public interface IFabrykaNPC
+    {
+        INPC CreateNPC();
+    }
+}
