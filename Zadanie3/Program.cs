df737be25935132491624f408a3bb5f3aad2389f
Jakub Matysek <148622115+ENRAEGE@users.noreply.github.com>
2025-04-05 09:44:57 +0200

    Add files via upload

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Paczkens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var zarzadzaniePaczkami = ZarządzaniePaczkami.Instancja;
+
+            
+            IFabrykaPaczek fabrykaDużych = new FabrykaDużychPaczek();
+            IFabrykaPaczek fabrykaMałych = new FabrykaMałychPaczek();
+
+         
+            IPaczka paczkaMała1 = fabrykaMałych.UtwórzPaczkę();
+            paczkaMała1.Przygotuj();
+
+            IPaczka paczkaMała2 = fabrykaMałych.UtwórzPaczkę();
+            paczkaMała2.Przygotuj();
+
+            
+            IPaczka paczkaDuża1 = fabrykaDużych.UtwórzPaczkę();
+            paczkaDuża1.Przygotuj();
+
+            IPaczka paczkaDuża2 = fabrykaDużych.UtwórzPaczkę();
+            paczkaDuża2.Przygotuj();
+        }
+    }
+
+    public interface IPaczka
+    {
+        void Przygotuj();
+    }
+
+    class MałaPaczka : IPaczka
+    {
+        public void Przygotuj()
+        {
+            Console.WriteLine("Przygotowano małą paczkę.");
+        }
+    }
+
+    class DużaPaczka : IPaczka
+    {
+        public void Przygotuj()
+        {
+            Console.WriteLine("Przygotowano dużą paczkę.");
+        }
+    }
+
+    class ZarządzaniePaczkami
+    {
+        private IFabrykaPaczek fabrykaPaczek;
+        private static ZarządzaniePaczkami _instancja;
+        private ZarządzaniePaczkami() 
+        {
+        }
+        public static ZarządzaniePaczkami Instancja
+        {
+            get
+            {
+                if (_instancja == null)
+                {
+                    _instancja = new ZarządzaniePaczkami();
+                }
+                return _instancja;
+            }
+        }
+    }
+
+    interface IFabrykaPaczek
+    {
+        IPaczka UtwórzPaczkę();
+    }
+
+    class FabrykaDużychPaczek : IFabrykaPaczek
+    {
+        public IPaczka UtwórzPaczkę()
+        {
+            return new DużaPaczka();
+        }
+    }
+
+    class FabrykaMałychPaczek : IFabrykaPaczek
+    {
+        public IPaczka UtwórzPaczkę()
+        {
+            return new MałaPaczka();
+        }
+    }
+}
