46045af3dd8e6c15aac99c3db0c8a7a463b3455b
Jakub Matysek <148622115+ENRAEGE@users.noreply.github.com>
2025-06-01 16:29:56 +0200

    Add files via upload

---
+++ Zadanie7/Zadanie7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal kwota = 10000m;
+
+            IPodatekStrategia podatekPolska = new PodatekPolska();
+            IPodatekStrategia podatekNiemcy = new PodatekNiemcy();
+
+            KalkulatorPodatku kalkulatorPolska = new KalkulatorPodatku(podatekPolska);
+            KalkulatorPodatku kalkulatorNiemcy = new KalkulatorPodatku(podatekNiemcy);
+
+            Console.WriteLine($"Podatek w Polsce od kwoty {kwota} wynosi: {kalkulatorPolska.ObliczPodatek(kwota)}");
+            Console.WriteLine($"Podatek w Niemczech od kwoty {kwota} wynosi: {kalkulatorNiemcy.ObliczPodatek(kwota)}");
+        }
+    }
+
+    public interface IPodatekStrategia
+    {
+        decimal ObliczPodatek(decimal kwota);
+    }
+
+    public class PodatekPolska : IPodatekStrategia
+    {
+        decimal IPodatekStrategia.ObliczPodatek(decimal kwota)
+        {
+            decimal kwotaPodatku = kwota * 0.23M;
+            return kwotaPodatku;
+        }
+    }
+
+    public class PodatekNiemcy : IPodatekStrategia
+    {
+        decimal IPodatekStrategia.ObliczPodatek(decimal kwota)
+        {
+            decimal kwotaPodatku = kwota * 0.19M;
+            return kwotaPodatku;
+        }
+    }
+
+    public class KalkulatorPodatku
+    {
+        private readonly IPodatekStrategia _strategia;
+
+        public KalkulatorPodatku(IPodatekStrategia strategia)
+        {
+            _strategia = strategia;
+        }
+
+        public decimal ObliczPodatek(decimal kwota)
+        {
+            return _strategia.ObliczPodatek(kwota);
+        }
+    }
+}
